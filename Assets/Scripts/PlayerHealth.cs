764a446998ea519d9442dc57f2e6f2406ae346d5
TRyc3 <btanaka808@gmail.com>
2022-10-04 18:39:46 -0700

    Player Health Bar, Damage
    -Added a player health bar. Currently a clone of the boss health bar.
    -Player sprite now briefly changes color upon taking damage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,6 +7,8 @@ public class PlayerHealth : MonoBehaviour
 {
     public int Health;
     public Text HealthText;
+    public HealthBar HealthBar;
+    private SpriteRenderer sprite;
 
     private int _plasticHealthLoss = 10;
     private int _urchinHealthLoss = 20;
@@ -15,14 +17,17 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        sprite = GetComponent<SpriteRenderer>();
         Health = 100;
+        HealthBar.set_max_health(100);
         HealthText.text = "Health: " + Health;
     }
 
     public void ReduceHealth(int i)
     {
+        sprite.color = new Color(1,0,0,1);
         Health = Health - i;
-        
+        HealthBar.set_health(Health);        
 
         if (Health <= 0)
         {
@@ -31,6 +36,11 @@ public class PlayerHealth : MonoBehaviour
         }
 
         HealthText.text = "Health: " + Health;
+        Invoke("ResetColor", .25f);
+    }
+
+    void ResetColor(){
+        sprite.color = new Color(1,1,1,1);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
