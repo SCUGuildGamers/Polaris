bfdc2fa0d721a0d9fddeea6de38780cd05aab314
SCU Imaginarium (Charlie) <>
2023-03-01 17:34:58 -0800

    Basic implementation

--- Assets/Scripts/LevelDesign/Pipe.cs
+++ Assets/Scripts/LevelDesign/Pipe.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Pipe : MonoBehaviour
 {
+    // Pipe state
     private bool isClogged;
+
+    // Reference for convenience
+    private Plastic plastic;
 
     // Direction of projectile shooting ("Up", "Right", "Down", "Left")
     [SerializeField]
@@ -15,7 +19,10 @@ public class Pipe : MonoBehaviour
 
     private void Start()
     {
+        plastic = FindObjectOfType<Plastic>();
+
         isClogged = false;
+
         StartCoroutine(StartShooting());
     }
 
@@ -25,11 +32,33 @@ public class Pipe : MonoBehaviour
         while (true) 
         {
             Shoot();
+
+            // Wait till period ends
             yield return new WaitForSeconds(period);
         }
     }
 
+    // Shoots a projectile
     void Shoot() {
-        Debug.Log("Shoot");
+        // Projectile direction
+        Vector3 proj_direction = GetDirection() + transform.position;
+
+        // Spawn/shoot the projectile
+        plastic.Spawn(transform.position, proj_direction, 3);
+    }
+
+    // Returns the direction of the projectile
+    Vector3 GetDirection() {
+        if (direction == "Up")
+            return new Vector3(0, 1, 0);
+
+        else if(direction == "Right")
+            return new Vector3(1, 0, 0);
+
+        else if (direction == "Down")
+            return new Vector3(0, -1, 0);
+
+        else
+            return new Vector3(-1, 0, 0);
     }
 }
