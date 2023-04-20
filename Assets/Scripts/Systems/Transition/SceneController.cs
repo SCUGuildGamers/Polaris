117f3b53b8ab6403d245b0addd270dca5b2d5a34
SCU Imaginarium (Zelda) <>
2023-04-19 17:31:10 -0700

    Functional transition scene

--- Assets/Scripts/Systems/Transition/SceneController.cs
+++ Assets/Scripts/Systems/Transition/SceneController.cs
@@ -12,7 +12,6 @@ public class SceneController : MonoBehaviour
     public void ChangeScene(string next_scene_string) {
         // Transfer the next scene string and quote so the transition properly works
         playerData.next_scene_string = next_scene_string;
-        playerData.transition_quote_text = "Hi";
 
         // Load the transition scene
         SceneManager.LoadScene(transition_scene_string);
--- Assets/Scripts/Systems/Transition/TransitionSceneHandler.cs
+++ Assets/Scripts/Systems/Transition/TransitionSceneHandler.cs
@@ -15,15 +15,19 @@ public class TransitionSceneHandler : MonoBehaviour
     // Quote text
     public Text quote_text;
 
+    private List<string> quote_list;
+
     private void Start()
     {
+        quote_list = new List<string> {"Hello world1", "Hello world2"};
+
         StartCoroutine(RunTransition());
     }
 
     // Handles the running of the transition scene
     private IEnumerator RunTransition() {
         // Set the text for the quote
-        quote_text.text = playerData.transition_quote_text;
+        quote_text.text = GetRandomQuote();
 
         // Pause for transition
         yield return new WaitForSeconds(transition_time);
@@ -34,5 +38,12 @@ public class TransitionSceneHandler : MonoBehaviour
 
     private void LoadNext() {
         SceneManager.LoadScene(playerData.next_scene_string);
+    }
+
+    private string GetRandomQuote() {
+        // Generate a random integer to choose one of the quotes
+        int random_int = Random.Range(0, quote_list.Count);
+
+        return quote_list[random_int];
     }
 }
