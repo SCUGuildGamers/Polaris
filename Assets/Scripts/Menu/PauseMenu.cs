4851a0899ad1112dae75c96ae6a1b63ad4f0016d
SCU Imaginarium (Byleth) <>
2023-05-22 17:37:12 -0700

    Working on Pause Menu UI

--- Assets/Scripts/Menu/PauseMenu.cs
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -9,6 +9,7 @@ public class PauseMenu : MonoBehaviour
 	public static bool CanPause = true;
 	public GameObject pauseMenuUI;
 	public GameObject optionsMenuUI;
+	public GameObject controlsMenuUI;
 
 	// For reference
 	private SceneController sceneController;
@@ -25,29 +26,36 @@ public class PauseMenu : MonoBehaviour
 	{
 		// If no menu is open
 		if (!pauseMenuUI.activeSelf && !optionsMenuUI.activeSelf)
-        {
+		{
 			if (Input.GetKeyDown(KeyCode.Escape) && CanPause)
 			{
 				Pause();
 			}
-        }
+		}
 		// If Options Menu is open
 		else if (optionsMenuUI.activeSelf)
-        {
+		{
 			if (Input.GetKeyDown(KeyCode.Escape))
-            {
+			{
 				optionsMenuUI.SetActive(false);
 				pauseMenuUI.SetActive(true);
-            }
-        }
+			}
+		}
+		// If Controls Menu is open
+		else if (controlsMenuUI.activeSelf) {
+			if (Input.GetKeyDown(KeyCode.Escape)) {
+				controlsMenuUI.SetActive(false);
+				pauseMenuUI.SetActive(true);
+			}
+		}
 		// If Pause Menu is open
 		else
-        {
+		{
 			if (Input.GetKeyDown(KeyCode.Escape))
-            {
+			{
 				Resume();
-            }
-        }
+			}
+		}
 		
 	}
 
