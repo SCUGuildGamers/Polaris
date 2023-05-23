749a7baaff40f175f4c9810eb70ebb829bb57472
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2023-05-23 09:28:37 -0700

    Getting everything working

--- Assets/Scripts/Crafting/CraftingBible.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "CraftingBible")]
-public class CraftingBible : ScriptableObject
-{
-    public List<Item> recipes;
-}
