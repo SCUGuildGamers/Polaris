6a2650e7df2ead3e46d71362dc92a211ac751e4f
SCU Imaginarium (Byleth) <>
2023-05-26 15:21:03 -0700

    Update AudioManager.cs

--- Assets/Scripts/Systems/Audio/AudioManager.cs
+++ Assets/Scripts/Systems/Audio/AudioManager.cs
@@ -11,13 +11,17 @@ public class AudioManager : MonoBehaviour
 
     private void Awake()
     {
+        // Ensure a single instance of the audio manager
         if (instance == null)
             instance = this;
+
+        // If another instance exists, then delete the extra instance
         else {
             Destroy(gameObject);
             return;
         }
 
+        // Transferring of the audio manager to carry over music
         DontDestroyOnLoad(gameObject);
 
         foreach (Sound s in sounds) {
@@ -30,14 +34,32 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    // Searches and plays the associated audio clip
     public void Play(string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
+        // Search the sounds array for the sound
+        Sound s = System.Array.Find(sounds, sound => sound.name == name);
+
+        // If not found, then return out
         if (s == null) {
             Debug.Log("Sound: " + name + " not found!");
             return;
         }
 
         s.source.Play();
+    }
+
+    private void CreateTempSound(Sound s) {
+        // Instantiate an empty object
+        GameObject gameObject = new GameObject("SoundSource");
+
+        // Add audio source to the object
+        AudioSource audioSource = gameObject.AddComponent<AudioSource>();
+
+        // Destroy the object after its length is played; I NEED TO ADD THE CLIP DURATION
+        Destroy(gameObject, 0f);
+
+        // Play the sound; I NEED TO INSERT THE CLIP
+        audioSource.PlayOneShot();
     }
 }
