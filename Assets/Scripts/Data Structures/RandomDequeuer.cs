83029741fd2c8f52b50fbf0a8db41db06722ddd8
joshsj89 <joshsj89@gmail.com>
2023-05-29 08:54:28 -0700

    Create RandomDequeuer.cs

---
+++ Assets/Scripts/Data Structures/RandomDequeuer.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Random = System.Random; // distinguishes between System's and UnityEngine's Random class
+
+public class RandomDequeuer<T> : MonoBehaviour
+{
+    // Fields/Variables
+    private Queue<T> dequeueList;
+    
+    // Constructors
+    public RandomDequeuer(List<T> items)
+    {
+        ShuffleList(ref items);
+        
+        // Create a queue from the shuffled list
+        dequeueList = new Queue<T>(items);
+    }
+
+    public RandomDequeuer(RandomDequeuerData<T> dequeuerData)
+    {
+        ShuffleList(ref dequeuerData.oceanQuoteList);
+        dequeueList = new Queue<T>(dequeuerData.oceanQuoteList);
+    }
+
+    // Methods
+    private void ShuffleList(ref List<T> items)
+    {
+        Random rnd = new Random();
+
+        for (int i = items.Count - 1; i >= 0; i--)
+        {
+            int k = rnd.Next(i + 1);
+            T temp = items[i];
+            items[i] = items[k];
+            items[k] = temp;
+        }
+    }
+
+    public T Dequeue()
+    {
+        // If dequeueList is empty
+        if (dequeueList.Count == 0)
+            return default(T); // null for reference types, 0 for value types, etc.,
+
+        // Dequeue a random item
+        return dequeueList.Dequeue();
+    }
+}
