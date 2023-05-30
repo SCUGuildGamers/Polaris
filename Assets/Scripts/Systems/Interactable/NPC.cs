99580bc2eef6e3cea3f10a6bc4f3b96873bb4e72
Jake Tsuchiyama <jtsuchiyama@scu.edu>
2023-05-30 08:44:52 -0700

    NPC flipping to talk to player

--- Assets/Scripts/Systems/Interactable/NPC.cs
+++ Assets/Scripts/Systems/Interactable/NPC.cs
@@ -8,10 +8,37 @@ public class NPC : Interactable
     public Dialogue Dialogue;
     public NPCAddCharge npcAddCharge;
 
+    private bool _facingRight = true;
+
     protected override void OnInteract() 
     {
+        // Play dialogue
         _dialogueManager.StartDialogue(Dialogue);
+
+        // Add glide charge if necessary
         if(npcAddCharge)
             npcAddCharge.GainCharge();
+
+        Vector3 player_position = FindObjectOfType<PlayerMovement>().gameObject.transform.position;
+        Vector3 diff_vector = transform.position - player_position;
+
+        // Flip the NPC to face the player
+        if (diff_vector.x > 0 && _facingRight)
+            Flip();
+
+        else if (diff_vector.x < 0 && !_facingRight)
+            Flip();
+    }
+
+    // Flips the horizontal orientation of the character
+    public void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        _facingRight = !_facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
